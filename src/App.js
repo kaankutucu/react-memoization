7f0fe43cca5a5602f1cd3348a6abb559599d1ffa
Kaan <49414441+kaankutucu@users.noreply.github.com>
2022-04-10 12:13:35 +0300

    useCallback hook added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useMemo, useCallback } from "react";
+import Header from './components/header'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [number, setNumber] = useState(0);
+    const increment = useCallback(() => {setNumber(prevState => prevState + 1)} ,[])
+
+    return (
+        <div className="App">
+            <Header increment = {increment} />
+            <hr/>
+            <h1>{number}</h1>
+
+        </div>
+    );
 }
+
+
 
 export default App;
